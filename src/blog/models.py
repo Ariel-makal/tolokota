d6b2dc03bd808a4be846ce3ec818caade3035c48
Ariel Makal <makalyav6@gmail.com>
2024-05-21 14:33:57 +0100

    retirement du champs commentaire

--- src/blog/models.py
+++ src/blog/models.py
@@ -2,35 +2,7 @@ from django.db import models
 from  authenticate.models import User
 # Create your models here.
 
-class Post(models.Model):
-    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-    image = models.ImageField()
-    description = models.TextField(null=True, blank=True)
-    latitude = models.CharField(max_length=50, null=True)
-    longitude = models.CharField(max_length=50, null=True)
-    commenters = models.TextField(null=True, blank=True)
-    createdAt = models.DateField(auto_now=False, auto_now_add=False)
 
-    def __str__(self):
-        self.user
-
-    class Meta:
-        verbose_name = 'Post'
-        verbose_name_plural = 'Posts'
-        
-class Commentaire(models.Model):
-    post = models.ForeignKey(Post, verbose_name=("Son post"),null=True,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
-    createdAt = models.DateField(auto_now=False, auto_now_add=False)
-    contenu = models.TextField()
-    
-    def __str__(self):
-        pass
-
-    class Meta:
-        verbose_name = 'Commentaire'
-        verbose_name_plural = 'Commentaires'
-    
 class Zone(models.Model):
     label = models.TextField()
     perimeters = models.CharField(max_length=50, blank=True,null=True)
@@ -42,4 +14,31 @@ class Zone(models.Model):
         verbose_name = 'Zone'
         verbose_name_plural = 'Zones'
     
+class Post(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    image = models.ImageField()
+    description = models.TextField(null=True, blank=True)
+    latitude = models.CharField(max_length=50, null=True)
+    longitude = models.CharField(max_length=50, null=True)
+    createdAt = models.DateField(auto_now=False, auto_now_add=False)
+    zone = models.ForeignKey( Zone,on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        self.user
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        
+class Commentaire(models.Model):
+    post = models.ForeignKey(Post,null=True,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
+    createdAt = models.DateField(auto_now=False, auto_now_add=False)
+    contenu = models.TextField()
     
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'Commentaire'
+        verbose_name_plural = 'Commentaires'
