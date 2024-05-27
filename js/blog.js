a63ea313288e2dd9cc363077991a68ca27d28722
Daniel Zema <zema.d@infoset.cd>
2024-05-27 19:25:54 +0100

    get post - post data

--- js/blog.js
+++ js/blog.js
@@ -1,37 +1,98 @@
-
-console.log("ok")
 $.ajax({
 
-    url:"https://3ff5-2c0f-e00-640-8700-409a-2e2c-2794-dbbe.ngrok-free.app/api/posts",
-    type:"get",
-    headers:{
-        "ngrok-skip-browser-warning":true
+    url: "http://localhost:8000/api/posts?order[createdAt]=desc",
+    type: "get",
+    headers: {
+        "accept": "application/json"
     },
-    success:function(response){
-        console.log(response)
-        let feeds = "";
-        response.forEach(element => {
-            console.log(element);
-            feeds = feeds+feedTemplate(element)
-
+    success: function (res) {
+        console.log(res)
+        let feeds = ""
+        res.forEach(post => {
+            feeds += feedTemplate(post);
         });
-        $("#section-feeds").html(feeds)
+
+        $('#section-feeds').html(feeds);
+        console.log("fin")
     },
-    error:function(error){console.log(error)}
+    error: function (error) {
+        console.log(error)
+    }
 
-})
+});
 
 
-function feedTemplate(data){
-    
-    return html=` 
-    <div class="row">
-        <div class="col-1 ">
-            <img class=" rounded" src="${data.image}" alt="" style="width:60px; height:60px">
+function feedTemplate(_post) {
+    let html = ` 
+    <div class="row p-3">
+        <div class="card p-3 border-light" style="border-radius: 20px">
+            <div class="cadr-body service">
+                <div class="d-flex justify-content-between mb-3">
+                    <div>
+                        <span class="me-3 fw-bold text-dark">${_post.owner.nom}</span>.
+                        <span class="ms-3">@${_post.owner.pseudo}</span>
+                    </div>
+                    <div>
+                       <span style="font-size: 9pt">Depuis 
+                    `;
+                        if(moment().diff(moment(_post.createdAt), 'years') > 0) {
+                            html += `${moment().diff(moment(_post.createdAt), 'years')} ans`
+                        }
+                        else if (moment().diff(moment(_post.createdAt), 'months') > 0)
+                        {
+                            html += `${moment().diff(moment(_post.createdAt), 'months')} mois`
+                        }
+                        else if (moment().diff(moment(_post.createdAt), 'weeks') > 0)
+                        {
+                            html += `${moment().diff(moment(_post.createdAt), 'weeks')} sem.`
+                        }
+                        else if (moment().diff(moment(_post.createdAt), 'days') > 0)
+                        {
+                            html += `${moment().diff(moment(_post.createdAt), 'days')} jrs.`
+                        }
+                        else if (moment().diff(moment(_post.createdAt), 'hours') > 0)
+                        {
+                            html += `${moment().diff(moment(_post.createdAt), 'hours')} h.`
+                        }
+                        else if (moment().diff(moment(_post.createdAt), 'minutes') > 0)
+                        {
+                            html += `${moment().diff(moment(_post.createdAt), 'minutes')} min.`
+                        }
+                        else if (moment().diff(moment(_post.createdAt), 'seconds') > 0)
+                        {
+                            html += `${moment().diff(moment(_post.createdAt), 'seconds')} sec.`
+                        }
+               html+= `</span>
+                   </div>
+                </div>
+                <div class="mb-3">${_post.description}</div>
+                <div class="col-12 service-item mb-3">
+                    <img class="w-100" loading="lazy" src="${_post.image}" alt="" height="300" style="border-radius: 15px;object-fit: fill">
+                </div>
+                <div class="col-12">
+                    <button class="btn btn-sm btn-outline-dark" style="border-radius: 15px">
+                        <i class="fas fa-comment"></i> Commentaire ( ${_post.comments.length} )
+                    </button>
+                </div>
+            </div>
         </div>
-        <div class="col-11 px-3">
-            <p class="py-2  w-50 mb-3 mt-2" >${data.user}</p>
-            <p class="py-2  w-100"  >${data.description}</p>
-        </div>
-    </div>`
+    </div>`;
+return html;
+}
+
+const x = document.getElementById("demo");
+getLocation()
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    $('input[name="latitude"]').val(position.coords.latitude)
+    $('input[name="longitude"]').val(position.coords.longitude)
+    x.innerHTML = position.coords.latitude + " , " + position.coords.longitude;
 }
