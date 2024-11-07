4fd5495a467163147171ae7ca33c4ceec48a26d1
Arma <makalyav6@gmail.com>
2024-11-07 22:44:15 +0000

    Application pour l'envoie de l'email

--- src/sendemail/urls.py
+++ src/sendemail/urls.py
@@ -1,10 +1,8 @@
 
-from django.contrib import admin
 from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.contact_view, name='contact'),
+    path('Tolokota/contact-us', views.contact_view, name='contact'),
     path('confirmation/',views.confirmation_view, name='confirmation'),
 ]
