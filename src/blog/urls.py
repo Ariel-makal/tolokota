a92604d4edc1f45299e49f9956a2821c4e52ab7c
Arma <makalyav6@gmail.com>
2024-11-07 22:43:15 +0000

    Reliaison des !=pages et regler le lien vers d'autes pages

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,9 +2,11 @@ from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
     path('blog/',views.blog, name='blog'),
     path("about/",views.about, name="about"),
     path("maps/", views.maps, name="maps"),
+    #path("sign-up/",views.signup, name="sign"),
+    #path("login/",views.login, name="login")
 
 ]
